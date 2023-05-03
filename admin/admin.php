a9b0bc3edb88bcc0f54a048adfd269ae270d628c
AsifMahsud <asifmahsud543@gmail.com>
2023-05-03 06:03:13 +0500

    fix activation hook

--- admin/admin.php
+++ admin/admin.php
@@ -17,32 +17,6 @@ function sg_newsletter_settings_page()
     include SG_NEWSLETTER_PLUGIN . '/admin/admin_settings_page.php';
 }
 
-function subscribe_form_install()
-{
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'subscribe_form_options';
-    $charset_collate = $wpdb->get_charset_collate();
-    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
-      id mediumint(9) NOT NULL AUTO_INCREMENT,
-      image varchar(255) NOT NULL,
-      title varchar(255) NOT NULL,
-      description varchar(255) NOT NULL,
-      frequency varchar(255) NOT NULL,
-      PRIMARY KEY  (id)
-    ) $charset_collate;";
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
-}
-
-function subscribe_form_activation()
-{
-    // Run installation function
-    subscribe_form_install();
-    // Clear the permalinks after activation
-    flush_rewrite_rules();
-}
-register_activation_hook(__FILE__, 'subscribe_form_activation');
-
 function sg_newsletter_enqueue_admin_scripts($hook)
 {
     if ($hook === 'toplevel_page_sendgrid-newsletter-settings') {
